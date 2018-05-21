9255adcc3a30992e3a8d19b48e2ba135a5a90c2d
Amandeep <akleoaman@gmail.com>
2018-05-21 13:50:32 +0200

    9. Incorporating Router
    In this we use Router, Route and hashHistory library.

--- prefolder/src/index.js
+++ prefolder/src/index.js
@@ -2,12 +2,17 @@ import React from 'react'
 import {render} from 'react-dom'
 import './stylesheets/ui.scss'
 import { SkiDayList } from './components/SkiDayList'
-import { App } from './components/App';
+import { App } from './components/App'
+import { Router, Route, hashHistory } from 'react-router'
+import { Whoops404 } from './components/Whoops404'
 
 
 window.React = React
 
 render(
- <App/>,
+ <Router history={hashHistory}>
+     <Route path="/" component={App}/>
+     <Route path="*" component={Whoops404}/>
+   </Router>,
    document.getElementById('react-container')
 )
