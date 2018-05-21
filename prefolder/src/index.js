8473b036d1050a0e4d10534ffe73402de47e994b
Amandeep <akleoaman@gmail.com>
2018-05-21 11:22:27 +0200

    6.6 Displaying child components

--- prefolder/src/index.js
+++ prefolder/src/index.js
@@ -1,13 +1,32 @@
 import React from 'react'
 import {render} from 'react-dom'
-import { SkiDayCount } from './components/SkiDayCount'
+import { SkiDayList } from './components/SkiDayList'
 
 window.React = React
 
 render(
-  <SkiDayCount total={50}
-               powder={20}
-               backcountry={10}
-               goal={100}/>,
+  <SkiDayList days= {
+    [
+      {
+        resort: "Squaw Valley",
+        date: new Date("1/2/2016"),
+        powder: true,
+        backcountry: false
+      },
+      {
+        resort: "KrikWood",
+        date: new Date("3/12/2016"),
+        powder: false,
+        backcountry: false
+      },
+      {
+        resort: "Mt. Tallac",
+        date: new Date("1/2/2016"),
+        powder: true,
+        backcountry: true
+      }
+    ]
+  }/>,
+   
     document.getElementById('react-container')
 )
