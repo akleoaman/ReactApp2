df963d8a9670cd9fcd1d4ad17297fd98fbede10d
Amandeep <akleoaman@gmail.com>
2018-05-21 15:35:47 +0200

    9.4 Nesting Routes

--- prefolder/src/index.js
+++ prefolder/src/index.js
@@ -1,22 +1,7 @@
 import React from 'react'
-import {render} from 'react-dom'
-import './stylesheets/ui.scss'
-import { SkiDayList } from './components/SkiDayList'
-import { App } from './components/App'
-import { Router, Route, hashHistory } from 'react-router'
-import { Whoops404 } from './components/Whoops404'
-
+import { render } from 'react-dom'
+import routes from './routes'
 
 window.React = React
 
-render(
- <Router history={hashHistory}>
-     <Route path="/" component={App}/>
-     <Route path="list-days" component={App} >
-     <Route path=":filter" component={App} />
-     </Route>
-     <Route path="add-day" component={App}/>
-     <Route path="*" component={Whoops404}/>
-   </Router>,
-   document.getElementById('react-container')
-)
+render(routes, document.getElementById('react-container'))
