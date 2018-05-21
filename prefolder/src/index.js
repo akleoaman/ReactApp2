c8f12e56134e8973257a9b5f025cdafcbe83abdc
Amandeep <akleoaman@gmail.com>
2018-05-21 13:10:34 +0200

    8.2 Working with state
    we pass one state value

--- prefolder/src/index.js
+++ prefolder/src/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import {render} from 'react-dom'
 import './stylesheets/ui.scss'
 import { SkiDayList } from './components/SkiDayList'
+import { App } from './components/App';
 
 
 window.React = React
 
 render(
- <SkiDayList days= {[]}/>,
+ <App/>,
    
     document.getElementById('react-container')
 )
