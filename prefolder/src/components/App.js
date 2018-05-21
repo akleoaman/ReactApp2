b37cb18c7a161cab7fce5ff0f00c8669b7aee76f
Amandeep <akleoaman@gmail.com>
2018-05-21 14:11:39 +0200

    Setting up routes
    We add a Add a Day page using routing

--- prefolder/src/components/App.js
+++ prefolder/src/components/App.js
@@ -1,6 +1,7 @@
 import { Component } from 'react'
 import { SkiDayList } from './SkiDayList'
 import { SkiDayCount } from './SkiDayCount'
+import {AddDayForm } from "./AddDayForm"
 
 
 export class App extends Component{
@@ -46,17 +47,27 @@ export class App extends Component{
     {
         return(
             <div className="app">
-              <SkiDayList days={this.state.allSkiDays} />
-              <SkiDayCount total= {this.countDays()}
-              powder ={this.countDays(
-                  "powder"
-              )}
-                  backcountry= {this.countDays("backcountry")}
-               />
-            </div>
+            {(this.props.location.pathname === "/") ? 
+            <SkiDayCount total= {this.countDays()}
+            powder ={this.countDays(
+                "powder"
+            )}
+                backcountry= {this.countDays("backcountry")
+            }/> :
+                (this.props.location.pathname ==="/add-day") ?
+            <AddDayForm /> :
+        <SkiDayList days={this.state.allSkiDays}/>
+            
+
+            
+        }
+              
+        </div>
         )
     }
 }
+    
+
 
 
 
