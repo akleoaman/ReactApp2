b17521535bca0272a765ee2597b5f6210381e32e
Amandeep <akleoaman@gmail.com>
2018-05-26 01:18:14 +0200

    10.2 Updating Lifecycle
    In this we make changes like each person to become admin and remove admin with style changes on both.

--- prefolder/src/components/ui/Member.js
+++ prefolder/src/components/ui/Member.js
@@ -1,22 +1,47 @@
 import FaShield from 'react-icons/lib/fa/shield' 
 import { Component } from 'react'
 
-
 class Member extends Component {
 
-	render() {
-		console.log("i am starting member rendering")
-		const { name, thumbnail, email, admin, makeAdmin } = this.props
-		return (
-			<div className="member">
-				<h1>{name} {(admin) ? <FaShield /> : null}</h1>
-				<a onClick={makeAdmin}>Make Admin</a>
-				<img src={thumbnail} alt="profile picture" />
-				<p><a href={`mailto:${email}`}>{email}</a></p>
+  componentWillMount() {
+  	this.style = {
+  		backgroundColor: 'gray'
+  	}
+  }	
 
-			</div>
-		)
-	}
+  componentWillUpdate(nextProps) {
+    this.style = {
+      backgroundColor: (nextProps.admin) ? 'green' : 'purple'
+    }
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return this.props.admin !== nextProps.admin
+  }
+
+  render() {
+
+	const { name, thumbnail, email, admin, 
+          makeAdmin, removeAdmin } = this.props
+    return (
+        <div className="member" style={this.style}>
+        	<h1>{name} {(admin) ? <FaShield /> : null}</h1>
+          {(admin) ? 
+            <a onClick={() => removeAdmin(email)}>Remove Admin</a> :
+            <a onClick={() => makeAdmin(email)}>Make Admin</a>
+          }
+
+        	
+        	<img src={thumbnail} alt="profile picture" />
+        	<p><a href={`mailto:${email}`}>{email}</a></p>
+
+        </div>
+    )
+}
 }
 
 export default Member
+
+
+
+
